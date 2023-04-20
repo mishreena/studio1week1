e94c084dadd25d282f21dc31b9e4f84d26fbe762
mishreena <131338568+mishreena@users.noreply.github.com>
2023-04-20 16:45:33 +1000

    Add files via upload

---
+++ java/prereq/WordFrequencies.java
@@ -0,0 +1,23 @@
+package prereq;
+
+import java.util.Scanner;
+
+public class WordFrequencies {
+   public static void main(String[] args) {      
+      // STEP 1: Read the number of words into an integer
+
+      // STEP 2: Read the words, one-at-a-time into an ArrayList
+
+      // STEP 3: Find the number of times the *first* word appears in the ArrayList.
+      //         Print out this number in the required format.
+
+      // STEP 4: Question - What do you need to change in your Step 3 code
+      //                    to find the number of times the *second* word
+      //                    appears in the ArrayList?
+      //         Question - What about the *third* word?
+      //         Question - Do you see a pattern emerging?
+
+      // STEP 5: Turn your code in Step 3 into a loop that checks every word in the ArrayList
+
+   }
+}
